31ef73622d6d4a1879743d84bc1f334d7fe6e5b1
FrancoCorn <fcorn@fi.uba.ar>
2025-09-14 12:36:57 -0300

    Add basic coordinator and worker

--- coordinator/coordinator.go
+++ coordinator/coordinator.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net"
+	pb "tp1/protocol/messages"
+
+	"google.golang.org/grpc"
 )
 
+type Server struct {
+	pb.UnimplementedServerServer
+	assignedWorkers []string
+}
+
+func (s *Server) AskForWork(ctx context.Context, req *pb.ImFree) (*pb.AskForWorkResponse, error) {
+	if len(s.assignedWorkers) < 3 {
+		s.assignedWorkers = append(s.assignedWorkers, req.Content)
+		return &pb.AskForWorkResponse{Response: "Work"}, nil
+	} else {
+		return &pb.AskForWorkResponse{Response: "No work"}, nil
+	}
+}
+
 func main() {
-	fmt.Println("Coordinator!")
+	socketPath := "/tmp/mr-socket.sock"
+
+	lis, err := net.Listen("unix", socketPath)
+	if err != nil {
+		log.Fatalf("Error al escuchar: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterServerServer(grpcServer, &Server{})
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Error al servir: %v", err)
+	}
+
 }
--- worker/worker.go
+++ worker/worker.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"context"
+	"log"
+
+	pb "tp1/protocol/messages"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	fmt.Println("Worker!")
+	socketPath := "/tmp/mr-socket.sock"
+
+	conn, err := grpc.Dial( "unix://" + socketPath, grpc.WithInsecure())
+
+	client := pb.NewServerClient(conn)
+
+	resp, err := client.AskForWork(context.Background(), &pb.ImFree{Content: "Worker 1"})
+	if err != nil {
+		log.Fatalf("Error al solicitar trabajo: %v", err)
+	}
+
+	fmt.Println(resp.Response)
 }
